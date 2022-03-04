b91753d7773f76160e677ad2baa948a93eb3571a
petaryankov00 <petar741012@gmail.com>
2022-03-04 18:32:21 +0200

    Added GlobalConstants class

---
+++ src/FNews.Global/GlobalConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace FNews.Global
+{
+    public static class GlobalConstants
+    {
+        public const string ApiHeaderKey = "x-rapidapi-key";
+        public const string ApiHeaderHost = "x-rapidapi-host";
+        public const string ApiAuthToken = "81a0196ea706beae9db0ef083fc86090";
+        public const string ApiVersion = "v3.football.api-sports.io";
+
+    }
+}
