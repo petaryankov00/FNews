db4789cba1eb32e0858d2c1cc60c51f730e2e0c9
petaryankov00 <petar741012@gmail.com>
2022-03-08 01:28:32 +0200

    Change password requirements

--- src/FNews.Web/Program.cs
+++ src/FNews.Web/Program.cs
@@ -13,9 +13,18 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.SignIn.RequireConfirmedAccount = false;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
+
 builder.Services.AddRazorPages()
     .AddRazorRuntimeCompilation();
 
